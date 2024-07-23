e553fc3b25058df0892d1ad675049a42eda23963
Madpychosxop <xyzgaming155@gmail.com>
2024-07-23 14:36:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY MADPYCHOS16
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("MADPYCHOS16")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6694202933")
+
+
+
+
+
+
+
+  
